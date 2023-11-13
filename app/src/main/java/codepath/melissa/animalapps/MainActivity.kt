45027f41814d30ec5232878f432ba5d1eebe726f
Melissa Perez <melmperz@gmail.com>
2023-11-12 19:30:20 -0500

    Initial commit

---
+++ app/src/main/java/codepath/melissa/animalapps/MainActivity.kt
@@ -0,0 +1,11 @@
+package codepath.melissa.animalapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
